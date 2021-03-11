f411ab9bfe42157720174a12088975ff82f027c9
Matt Fellenz <mattf53190@gmail.com>
2021-03-11 07:02:15 -0800

    Local Makefiles for frontend and backend
    - Move existing Makefile to `frontend/`, and update `FRONTEND` var to
    `.`.
    - Update `nunjucksbuild.js` to take the template path as an argument to
    make it compatible with the move by taking `NUNJUCKS_TEMPLATES` as an
    argument.
    - Add a stub Makefile to `backend/`, with a `.PHONY` `all` target, that
    just runs a nop to make `make` happy. This file will be fleshed out as
    we begin working on the backend, or if no compilation/preprocessing is
    needed, will stay as-is (or be removed entirely, but I prefer keeping
    it personally).
    - Add a new super-Makefile to the top level that just runs sub-makes
    in the frontend and backend directories.

--- frontend/nunjucksbuild.js
+++ frontend/nunjucksbuild.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const nunjucks = require('nunjucks');
 const input = process.argv[2];
 const output = process.argv[3];
-const env = new nunjucks.Environment(new nunjucks.FileSystemLoader('frontend/src/templates/'), { 'lstripBlocks': true, 'trimBlocks': true, });
+const templates_dir = process.argv[4];
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(templates_dir), { 'lstripBlocks': true, 'trimBlocks': true, });
 fs.writeFileSync(
 	output,
 	nunjucks.compile(fs.readFileSync(input, 'utf8'), env).render({ 'filename': require('path').basename(input, '.njk'), }),
